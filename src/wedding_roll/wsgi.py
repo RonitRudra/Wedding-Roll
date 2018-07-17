55ab57be5b944e2627f59cb95a6945797e77727f
Ronit Rudra <ronit.rudra1995@gmail.com>
2018-07-17 01:22:55 -0500

    changed wsgi settings

--- src/wedding_roll/wsgi.py
+++ src/wedding_roll/wsgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wedding_roll.settings")
+# use "gunicorn --bind 0.0.0.0:8000 wedding_roll.wsgi" to run server via gunicorn
+# Gunicorn should always be used to deploy production servers.
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wedding_roll.settings.production")
 
 application = get_wsgi_application()
